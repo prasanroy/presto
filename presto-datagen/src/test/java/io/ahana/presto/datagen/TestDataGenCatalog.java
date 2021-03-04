12cdc67258454f9e796758abbe70d3a5cb8d94df
Prasan Roy <prasan@ahana.io>
2021-03-04 13:42:00 +0530

    Change the type of TEST_CATALOG to DataGenCatalog

--- presto-datagen/src/test/java/io/ahana/presto/datagen/TestDataGenCatalog.java
+++ presto-datagen/src/test/java/io/ahana/presto/datagen/TestDataGenCatalog.java
@@ -17,7 +17,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import org.testng.annotations.Test;
 
-import java.util.List;
 import java.util.Optional;
 
 import static com.facebook.presto.common.type.BigintType.BIGINT;
@@ -32,12 +31,11 @@ public class TestDataGenCatalog
     public void testMetadata()
             throws Exception
     {
-        DataGenCatalog dataGenCatalog = new DataGenCatalog(TEST_CATALOG);
-        assertEquals(dataGenCatalog.getSchemaNames(), ImmutableSet.of("first", "second"));
-        assertEquals(dataGenCatalog.getTableNames("first"), ImmutableSet.of("ta", "tb"));
-        assertEquals(dataGenCatalog.getTableNames("second"), ImmutableSet.of("tb"));
+        assertEquals(TEST_CATALOG.getSchemaNames(), ImmutableSet.of("first", "second"));
+        assertEquals(TEST_CATALOG.getTableNames("first"), ImmutableSet.of("ta", "tb"));
+        assertEquals(TEST_CATALOG.getTableNames("second"), ImmutableSet.of("tb"));
 
-        Optional<DataGenTable> tableOpt = dataGenCatalog.getTable("first", "ta");
+        Optional<DataGenTable> tableOpt = TEST_CATALOG.getTable("first", "ta");
         assertTrue(tableOpt.isPresent(), "Table 'ta' is not present");
 
         DataGenTable table = tableOpt.get();
@@ -45,51 +43,52 @@ public class TestDataGenCatalog
         assertEquals(table.getColumns(), ImmutableList.of(new DataGenColumn("x", BIGINT), new DataGenColumn("y", DOUBLE)));
     }
 
-    public static final List<DataGenSchema> TEST_CATALOG = ImmutableList.of(
-            new DataGenSchema(
-                "first",
-                ImmutableList.of(
-                    new DataGenTable(
-                        "ta",
-                        ImmutableList.of(
-                            new DataGenColumn("x", BIGINT),
-                            new DataGenColumn("y", DOUBLE)),
-                        ImmutableList.of(
-                            new DataGenTableStats(
-                                10,
-                                ImmutableList.of(
-                                    new DataGenColumnStats("x", 1, 3, 2),
-                                    new DataGenColumnStats("y", 5.5, 7.0, 3))),
-                            new DataGenTableStats(
-                                4,
-                                ImmutableList.of(
-                                    new DataGenColumnStats("x", 4, 9, 3),
-                                    new DataGenColumnStats("y", 0.0, 9.0, 5))))),
-                    new DataGenTable(
-                        "tb",
-                        ImmutableList.of(
-                            new DataGenColumn("u", INTEGER)),
-                        ImmutableList.of(
-                            new DataGenTableStats(
-                                10,
-                                ImmutableList.of(
-                                    new DataGenColumnStats("u", 1, 3, 2))),
-                            new DataGenTableStats(
-                                5,
-                                ImmutableList.of(
-                                    new DataGenColumnStats("u", 4, 9, 3))))))),
-            new DataGenSchema(
-                "second",
-                ImmutableList.of(
-                    new DataGenTable(
-                        "tb",
-                        ImmutableList.of(
-                            new DataGenColumn("u", INTEGER),
-                            new DataGenColumn("v", DOUBLE)),
-                        ImmutableList.of(
-                            new DataGenTableStats(
-                                10,
-                                ImmutableList.of(
-                                    new DataGenColumnStats("u", 1, 3, 2),
-                                    new DataGenColumnStats("v", 5.5, 7.0, 3))))))));
+    public static final DataGenCatalog TEST_CATALOG = new DataGenCatalog(
+            ImmutableList.of(
+                new DataGenSchema(
+                    "first",
+                    ImmutableList.of(
+                        new DataGenTable(
+                            "ta",
+                            ImmutableList.of(
+                                new DataGenColumn("x", BIGINT),
+                                new DataGenColumn("y", DOUBLE)),
+                            ImmutableList.of(
+                                new DataGenTableStats(
+                                    10,
+                                    ImmutableList.of(
+                                        new DataGenColumnStats("x", 1, 3, 2),
+                                        new DataGenColumnStats("y", 5.5, 7.0, 3))),
+                                new DataGenTableStats(
+                                    4,
+                                    ImmutableList.of(
+                                        new DataGenColumnStats("x", 4, 9, 3),
+                                        new DataGenColumnStats("y", 0.0, 9.0, 4))))),
+                        new DataGenTable(
+                            "tb",
+                            ImmutableList.of(
+                                new DataGenColumn("u", INTEGER)),
+                            ImmutableList.of(
+                                new DataGenTableStats(
+                                    10,
+                                    ImmutableList.of(
+                                        new DataGenColumnStats("u", 1, 3, 2))),
+                                new DataGenTableStats(
+                                    5,
+                                    ImmutableList.of(
+                                        new DataGenColumnStats("u", 4, 9, 3))))))),
+                new DataGenSchema(
+                    "second",
+                    ImmutableList.of(
+                        new DataGenTable(
+                            "tb",
+                            ImmutableList.of(
+                                new DataGenColumn("u", INTEGER),
+                                new DataGenColumn("v", DOUBLE)),
+                            ImmutableList.of(
+                                new DataGenTableStats(
+                                    10,
+                                    ImmutableList.of(
+                                        new DataGenColumnStats("u", 1, 3, 2),
+                                        new DataGenColumnStats("v", 5.5, 7.0, 3)))))))));
 }
